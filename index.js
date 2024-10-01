421c76124f8082c1ed3adafbbfb1b131fbdaeece
davidgar026 <130368930+davidgar026@users.noreply.github.com>
2024-09-30 21:07:40 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+let postArr = [];
 
 app.use(bodyParser.urlencoded({extended: true }));
 app.use(express.static("public"));
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", 
+        {data: postArr}
+    );
 });
 
 app.get("/about", (req, res) => {
@@ -23,8 +27,17 @@ app.get("/createposts", (req, res) => {
     res.render("createposts.ejs");
 })
 
-app.post("/submit", (req, res) => {
-    res.send(req.body.postInput)
+app.post("/submit", (req, res, next) => {
+    let {postInput} = req.body;
+    postArr.push(postInput);
+    console.log("query = ", req.query.postArr)
+    res.redirect('/')
+    //res.render("index.ejs", {data: postArr})
+    
+    
+    
+    
+    //postArr = [];
 })
 
 
